8e89e83073fafcd448cacaf7afd3e03a68980478
Denise <denise@decimalpoint.com.cn>
2025-01-03 21:09:33 +0800

    feat(algorithms): bubble sort

---
+++ docs/algorithms/code/sort-algorithm/bubble-sort.ts
@@ -0,0 +1,16 @@
+// 冒泡排序
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 解读：冒泡排序的思想是每次从未排序区间中找到最大值，然后放到已排序区间的末尾。
